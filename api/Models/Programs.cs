2523034c038e2f7e4ef8089c93a980bf93fc2949
Harsh Rastogi <rastogiharsh04@gmail.com>
2023-05-08 19:35:05 +0530

    remove generated files

---
+++ api/Models/Programs.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Powerhouse_Fitness.Models
+{
+    public class Programs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProgramId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Duration is required")]
+        public string Duration { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Cost is required")]
+        [DataType(DataType.Currency)]
+        public decimal Cost { get; set; }
+
+    }
+}
